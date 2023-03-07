3bb01c068a4b650a59e7cad69878819b327169db
JoseMRodriguez <Josemiguelrodriguez89@gmail.com>
2023-03-06 21:27:02 -0600

    Change search base on params instead of hard-coded search

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,6 @@
 class ArticlesController < ApplicationController
   def index 
-    response = HTTP.get("https://newsapi.org/v2/everything?q=tesla&apiKey=#{ENV["NEWS_API_KEY"]}")
+    response = HTTP.get("https://newsapi.org/v2/everything?q=#{params[:search_terms]}&apiKey=#{ENV["NEWS_API_KEY"]}")
     articles = response.parse
     render json:articles
   end
