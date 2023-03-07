c0ba22126a0c1e965700b239c2bc12036f040aac
JoseMRodriguez <Josemiguelrodriguez89@gmail.com>
2023-03-06 21:24:53 -0600

    news back end working with protected Api key on ENV

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,7 @@
+class ArticlesController < ApplicationController
+  def index 
+    response = HTTP.get("https://newsapi.org/v2/everything?q=tesla&apiKey=#{ENV["NEWS_API_KEY"]}")
+    articles = response.parse
+    render json:articles
+  end
+end
